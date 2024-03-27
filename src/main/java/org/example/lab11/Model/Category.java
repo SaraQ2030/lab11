ea91ddf767f82fffe7567926c775527ddc6f4e55
Sara_Alqahtani <159685978+SaraQ2030@users.noreply.github.com>
2024-03-27 19:52:45 +0300

    Add files via upload

---
+++ src/main/java/org/example/lab11/Model/Category.java
@@ -0,0 +1,21 @@
+package org.example.lab11.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "name cannot be empty")
+    @Column(columnDefinition = "varchar(10) not null unique")
+    private String name;
+}
